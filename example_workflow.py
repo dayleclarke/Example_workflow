3979788e405466d3077e6893c599e17a6f0bc271
Dayle Clarke <dayleclarke07@gmail.com>
2022-10-11 20:46:11 +1000

    example descriptive commit message

--- example_workflow.py
+++ example_workflow.py
@@ -1,2 +1,2 @@
-print('This is the main branch')
+print('This is the example work in progress login module')
 print('This is an example test workflow to demonstrate the functionality of the Feature Branching Workflow')
